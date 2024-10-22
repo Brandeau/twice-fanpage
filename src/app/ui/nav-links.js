4ced4ef47e359b8f25a7c592c697c40b83e50c5e
Brandeau <diego.andrade.y@gmail.com>
2024-10-22 13:40:39 -0300

    feat: create side navbar

--- src/app/ui/nav-links.js
+++ src/app/ui/nav-links.js
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import clsx from 'clsx';
+import { inter } from "./fonts";
 
 const links = [
     {
@@ -20,4 +21,30 @@ const links = [
     {
         name: 'Reality', href: '/reality'
     },
-]
+];
+
+export default function NavLinks() {
+    const pathname = usePathname();
+    return (
+      <>
+        {links.map((link) => {
+          {/*const LinkIcon = link.icon;*/}
+          return (
+            <Link
+              key={link.name}
+              href={link.href}
+              className={clsx(
+                'flex w-1/6 h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+                {
+                  'bg-sky-100 text-blue-600': pathname === link.href,
+                },
+              )}
+            >
+              {/*<LinkIcon className="w-6" />*/}
+              <p className={`${inter.className} block`}>{link.name}</p>
+            </Link>
+          );
+        })}
+      </>
+    );
+  }
--- src/app/ui/sidenav.js
+++ src/app/ui/sidenav.js
@@ -1,8 +1,12 @@
 import Link from "next/link";
+import NavLinks from "./nav-links";
 
 export default function Sidenav(){
     return(
         <>
+            <div className="flex grow flex-col justify-start space-x-0 md:flex-col md:space-x-0 md:space-y-2">
+                <NavLinks />
+            </div>
         </>
     )
 }
