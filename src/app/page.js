562c8e387ed5c45a4ba11aef53f6e2dd4a7c86a9
Brandeau <diego.andrade.y@gmail.com>
2024-10-24 12:01:28 -0300

    refactor: make card section iterative

--- src/app/page.js
+++ src/app/page.js
@@ -4,15 +4,23 @@ import { lusitana } from "./ui/fonts";
 import between12  from '../public/between12.jpg'
 import { Card } from "./ui/cards";
 
+const sections = [
+  {title: 'albums', src:between12, description:"album cover"},
+  {title: 'story', src:between12, description:"story cover"},
+  {title: 'members', src:between12, description:"members cover"},
+  {title: 'reality', src:between12, description:"realities cover"}
+]
+
 export default function Page() {
-  return (
-    <>
-      <main className="grid md:grid-cols-2 sm:grid-cols-1 self-center">
-        <Card title="albums" src={between12}  description="album cover"></Card>
-        <Card title="members" src={between12}  description="album cover"></Card>
-        <Card title="story" src={between12}  description="album cover"></Card>
-        <Card title="reality" src={between12} description="album cover"></Card>
-      </main>
-    </>
-  );
+    return (
+      <>
+        <main className="grid md:grid-cols-2 sm:grid-cols-1 self-center">
+        {sections.map((section) =>{
+          return(
+          <Card title={section.title} src={section.src}  description={section.description}></Card>
+          );
+        })}
+        </main>
+      </>
+    );
 }
