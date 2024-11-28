c71a34e67099321477910bba11763300eeed2b94
Brandeau <diego.andrade.y@gmail.com>
2024-11-28 12:31:06 -0300

    feat: add page component for individual albums

---
+++ src/app/albums/[id]/page.js
@@ -0,0 +1,55 @@
+'use client'
+
+import { useState, useEffect } from "react";
+
+/**
+ * A Next.js page component that fetches release group data based on the provided `id` parameter.
+ * @param {object} param0
+ * @returns {JSX.Element}
+ */
+export default function Page({ params }){
+
+    const [releaseGroups, setReleaseGroups] = useState(null);
+
+    useEffect(() => {
+        async function fetchReleaseGroups(){
+            const res = await fetch(`/api/release_groups/${params.id}`);
+            const data = await res.json();
+            console.log({ res, data })
+            setReleaseGroups(data)
+        };
+
+        fetchReleaseGroups(); 
+    }, []);
+
+    if (!releaseGroups) return <div>Loading...</div>
+
+    return(
+
+        <>
+            <h1>Versions</h1>
+            <br></br>
+
+            {releaseGroups.releases.map((release) => {
+                return(
+                    <>
+                        <p>{release.title}</p> 
+                        <p>{release.release_year}</p>
+                        <p>{release.countries.name}</p>
+                        <p>{release.release_formats?.format}</p>
+                        <p>{release.annotation}</p>
+                        <br></br>
+                        <h2>Tracklist</h2>
+                        <br></br>
+                        {release.releases_tracks.map((releases_track) =>{
+                            return (
+                            <p>{releases_track.tracks.title}</p>
+                            )
+                        })}
+                        <br></br>
+                    </>
+                );
+            })}
+        </>
+    )
+}
