82ccd79eb13b91c3200390a4fef0216cb070225f
Brandeau <diego.andrade.y@gmail.com>
2024-11-28 12:30:16 -0300

    feat: create page for all albums

--- src/app/albums/page.js
+++ src/app/albums/page.js
@@ -1,21 +1,35 @@
 'use client'
 
 import { Card } from "../ui/cards";
-import momo from '../../public/momo.jpg'
 import { usePathname } from "next/navigation";
+import { useState, useEffect } from "react";
 
-const albums = [
-    {href:"ttt", src:momo, title:"Talk That Talk",description:"ttt"},
-    {href:"eyes", src:momo, title:"Eyes Wide Open",description:"eyes"},
-]
-
+/**
+ * A Next.js page component that fetches release group data based on the provided `id` parameter.
+ * @returns {JSX.Element} The rendered component with release group data or a loading message.
+ */
 export default function Page(){
+
+    const [releaseGroups, setReleaseGroups] = useState(null);
+
+    useEffect(() => {
+        async function fetchReleaseGroups(){
+            const res = await fetch(`/api/release_groups/`);
+            const data = await res.json();
+            console.log({ res, data })
+            setReleaseGroups(data)
+        };
+
+        fetchReleaseGroups(); 
+    }, []);
+
+    if (!releaseGroups) return <div>Loading...</div>
     const path = usePathname();
     return(
         <>
-            {albums.map((album) =>{
+            {releaseGroups.res.map((releaseGroup) =>{
             return(
-                <Card title={album.title} src={album.src} href={`${path}/${album.href}`} description={album.description}></Card>
+                <Card title={releaseGroup.name} src={`/images/releases/${releaseGroup.id}.webp`} href={`${path}/${releaseGroup.id}`} description="album cover"></Card>
                 );
             })}
         </>
