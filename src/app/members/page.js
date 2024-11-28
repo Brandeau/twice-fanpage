fe4dfbc7a821eb3c763758a0d62d2bf3986f49f1
Brandeau <diego.andrade.y@gmail.com>
2024-11-28 12:07:21 -0300

    feat: create all members page

--- src/app/members/page.js
+++ src/app/members/page.js
@@ -1,21 +1,19 @@
 'use client'
 
 import { Card } from "../ui/cards";
-import momo from '../../public/momo.jpg'
 import { usePathname } from "next/navigation";
 
-const members = [
-    {href:"momo", src:momo, title:"momo",description:"momo"},
-    {href:"nayeon", src:momo, title:"nayeon",description:"nayeon"},
-]
+const res = await fetch('/api/members');
+const members = await res.json();
+
 
 export default function Page(){
     const path = usePathname();
     return(
         <>
-            {members.map((member) =>{
+            {members.res.map((member) =>{
             return(
-                <Card title={member.title} src={member.src} href={`${path}/${member.href}`} description={member.description}></Card>
+                <Card title={member.first_name} src={`/images/members/${member.id}.jpg`} href={`${path}/${member.id}`} description={member.id}></Card>
                 );
             })}
         </>
