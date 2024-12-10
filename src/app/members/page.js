a0c78c179a7dc5221ebdd7ef6dbb6550c6fbf05b
Brandeau <diego.andrade.y@gmail.com>
2024-12-10 11:55:52 -0300

    refactor: move fetch call to inside useeffect

--- src/app/members/page.js
+++ src/app/members/page.js
@@ -2,13 +2,26 @@
 
 import { Card } from "../ui/cards";
 import { usePathname } from "next/navigation";
-
-const res = await fetch('/api/members');
-const members = await res.json();
+import { useState, useEffect } from "react";
 
 
 export default function Page(){
+    const [members, setMembers] = useState(null);
+
+    useEffect(() => {
+
+        async function fetchMembers(){
+            const res = await fetch(`/api/members/`);
+            const data = await res.json();
+            setMembers(data)
+        };
+
+        fetchMembers(); 
+    }, []);
+
     const path = usePathname();
+    if (!members) return <div>Loading...</div>
+
     return(
         <div className="grid md:grid-cols-3 sm:grid-cols-1 self-center ">
                 {members.res.map((member, index) =>{
